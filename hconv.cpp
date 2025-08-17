ee42444835152037d885cfe5aa1a58f2301e749e
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-08-17 18:15:29 -0400

    Create hconv.cpp

---
+++ hconv.cpp
@@ -0,0 +1,13 @@
+//// HEX CONVERTER 
+
+
+#include <iostream> 
+
+
+
+int main(int argc, char *argv[]) { 
+  if ((argc>1)&&argv[1]=='s'||argv[1]=='-s') { 
+    std::cout << " "; 
+  } 
+  return 0; 
+} 
